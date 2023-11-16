14d7ab88bf00a4e7378bf58dd748865163dc4dbc
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-11-16 01:00:40 -0300

    v 0.2 test bench

--- tb_multiplier.vhd
+++ tb_multiplier.vhd
@@ -11,11 +11,11 @@ end entity;
 
 architecture testbench of multiplier_tb is
 
-    signal clk, habilitacion : std_logic := '0';
+    signal clk, st : std_logic := '0';
 
-    signal multiplicando, multiplicador : std_logic_vector(3 downto 0);
-    signal fin_multipli : std_logic;
-    signal vector_resultado : std_logic_vector(9 downto 0);
+    signal mplier, mcand : std_logic_vector(3 downto 0);
+    signal done : std_logic;
+    signal product : std_logic_vector(9 downto 0);
 
     constant N : natural := 3;  -- cambio segun lo deseado
 
@@ -29,29 +29,29 @@ architecture testbench of multiplier_tb is
             
             port map (
                 clk => clk,
-                habilitacion => habilitacion,
-                multiplicando => multiplicando,
-                multiplicador => multiplicador,
-                fin_multipli => fin_multipli,
-                vector_resultado => vector_resultado
+                st => st,
+                mplier => mplier,
+                mcand => mcand,
+                done => done,
+                product => product
             );
 
         process
 
             begin
                 --0 multiplicado por 1
-                multiplicando <= "0000";
-                multiplicador <= "0001";
+                mplier <= "0000";
+                mcand <= "0001";
                 wait for 10 ns;
 
                 --1111 multiplicado por 1111
-                multiplicando <= "1111";
-                multiplicador <= "1111";
+                mplier <= "1111";
+                mcand <= "1111";
                 wait for 10 ns;
 
                 --13 multiplicado por 11
-                multiplicando <= "1101";
-                multiplicador <= "1011";
+                mplier <= "1101";
+                mcand <= "1011";
                 wait for 10 ns;
 
                 --
