66ae9b740db047e24e17c48f0dd75c61eab9fea7
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-11-18 00:39:18 -0300

    test bench v2

--- multiplier_tb.vhd
+++ multiplier_tb.vhd
@@ -23,6 +23,7 @@ architecture testbench of multiplier_tb is
     signal done : STD_LOGIC;
     signal product : STD_LOGIC_VECTOR(2*M-1 downto 0);
     signal mplier, mcand : STD_LOGIC_VECTOR(M-1 downto 0);
+    signal numeros : STD_LOGIC_VECTOR(2*M-1 downto 0);
 
 begin
     COMPONENTE: multiplier
@@ -38,12 +39,11 @@ begin
             product => product
         );
 
-        prueba: process
+    prueba: process
         variable pass : BOOLEAN := TRUE;
     begin
-        wait for 10 ns;
-    
-        -- Casos de prueba específicos
+        wait for 10 ns; 
+        
         mplier <= "1111";  -- 1111 x 0000
         mcand <= "0000";
         st <= '1';
@@ -51,7 +51,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 0 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "1111";  -- 1111 x 1111
         mcand <= "1111";
         st <= '1';
@@ -59,7 +60,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 1 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "1101";  -- 13 x 11
         mcand <= "1011";
         st <= '1';
@@ -67,7 +69,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 2 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "1100";  -- 1100 x 0011
         mcand <= "0011";
         st <= '1';
@@ -75,7 +78,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 3 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "0001";  -- 0001 x 0001
         mcand <= "0001";
         st <= '1';
@@ -83,7 +87,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 4 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "0010";  -- 0010 x 0001
         mcand <= "0001";
         st <= '1';
@@ -91,7 +96,8 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 5 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         mplier <= "0010";  -- 0010 x 0010
         mcand <= "0010";
         st <= '1';
@@ -99,13 +105,13 @@ begin
         st <= '0';
         wait for 100 ns;
         wait until rising_edge(clk);
-    
+        report "DEBUG: i = 6 numeros = " & to_string(numeros) & " product = " & to_string(product) & " done = " & to_string(done) severity note;
+
         wait until done = '1';
-    
+
         report "DEBUG: Testbench completed." severity note;
-    
+
         wait;
     end process prueba;
-    
 
 end architecture testbench;
